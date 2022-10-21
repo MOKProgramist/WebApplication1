0991e23562c55b5767a3bf73232a976951d07f08
315 <315@315-13>
2022-10-21 11:26:34 +0300

    Добавьте файлы проекта.

---
+++ WebApplication1/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace WebApplication1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld/
+        public string Index()
+        {
+            return "Дефолтная страница";
+        }
+
+        // GET: HelloWorld/welcome
+        // Requires using System.Text.Encodings.Web
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
